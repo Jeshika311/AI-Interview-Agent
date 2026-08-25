18b126e8769a5de4d125de269d743c32745596ab
Jeshika <jeshikatha@gmail.com>
2026-08-25 06:33:45 +0530

    index file

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/connectDB.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import interviewRouter from "./routes/interviewRoutes.js";
+import paymentRouter from "./routes/paymentRoutes.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/interview", interviewRouter);
+app.use("/api/payment", paymentRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+  connectDB();
+});
